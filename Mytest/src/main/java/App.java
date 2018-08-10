6a6f097549984af99ef139c3b04d06f8866670cd
gujin <gujin_qd@caxins.com>
2018-08-10 17:42:41 +0800

    2

---
+++ Mytest/src/main/java/App.java
@@ -0,0 +1,13 @@
+
+/**
+ * @Description: Hallo World
+ * @Author: Gj
+ * @system name:
+ * @copyright：
+ * @Date: Created in  2018-08-10
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
