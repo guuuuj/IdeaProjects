7a8b2ea3b2bdccce1aac435e22f32ae0817c53e6
gujin <gujin_qd@caxins.com>
2018-08-16 17:57:04 +0800

    activemq-provider

---
+++ Remoteapi/src/main/java/cn/mldn/mqprovider/producer/IMessageProducer.java
@@ -0,0 +1,12 @@
+package cn.mldn.mqprovider.producer;
+
+/**
+ * @Description:
+ * @Author: Gj
+ * @system name:
+ * @copyright：（）
+ * @Date: Created in  2018-08-16
+ */
+public interface IMessageProducer {
+    public void sendMessage(String msg);
+}
---
+++ mqprovider/src/main/java/cn/mldn/mqprovider/producer/impl/MessageProducerImpl.java
@@ -0,0 +1,35 @@
+package cn.mldn.mqprovider.producer.impl;
+
+import cn.mldn.mqprovider.producer.IMessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.print.attribute.standard.Destination;
+
+/**
+ * @Description:
+ * @Author: Gj
+ * @system name:
+ * @copyright：（）
+ * @Date: Created in  2018-08-16
+ */
+@Component
+public class MessageProducerImpl implements IMessageProducer{
+    @Resource
+    private JmsTemplate jmsTemplate;
+    @Resource
+    private Destination destination;//设置消息目的地
+    public void sendMessage(String msg) {
+        this.jmsTemplate.send(this.destination,new MessageCreator() {
+            public Message createMessage(Session session) throws JMSException {
+                return session.createTextMessage(msg);
+            }
+        });
+    }
+}
